b9894e4a39b64bcbb4f52ede17f8ea1b2060496b
Jongheon Park <zxcv4306@gmail.com>
2022-03-31 12:37:45 -0400

    Added tb top

--- sources/example/tb_axi_slave_example.sv
+++ sources/example/tb_axi_slave_example.sv
@@ -1,5 +1,45 @@
-module tb_axi_slave_example;
-	axi_slave_example
+'timescale 1ns/1ps
 
-	AXI_LITE axi_lite ()
-endmodule
+module tb_axi_slave_example;
+	localparam TB_ADDR_WIDTH = 4,
+			   TB_DATA_WIDTH = 32;
+
+	localparam CLK_FREQ = 100; // MHz
+	localparam CLK_PERIOD = 1000/CLK_FREQ; 
+
+	bit tb_clk,
+		tb_rstn;
+
+	// CLOCK
+	initial begin
+		tb_clk = 0;
+		forever begin
+			#(CLK_PERIOD/2); 
+			tb_clk = ~tb_clk;
+		end
+	end
+
+	AXI_LITE #(
+		AXI_ADDR_WIDTH = TB_ADDR_WIDTH, 
+		AXI_DATA_WIDTH = TB_DATA_WIDTH
+	) tb_axi_l ();
+
+	test_axi_slave_example #(
+		ADDR_WIDTH = TB_ADDR_WIDTH,
+		DATA_WIDTH = TB_DATA_WIDTH
+	) test_axi_slave_example (
+		.clk_i(tb_clk),
+		.rstn_i(tb_rstn),
+		.axi_l(tb_axi_l.Master)
+	);
+
+	axi_slave_example #(
+		ADDR_WIDTH = TB_ADDR_WIDTH,
+		DATA_WIDTH = TB_DATA_WIDTH
+	) dfu_axi_slave (
+		.clk_i(tb_clk),
+		.rstn_i(tb_rstn),
+		.axi_l(tb_axi_l.Slave)
+	);
+
+endmodule : tb_axi_slave_example
