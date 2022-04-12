0fd2582f2a0f7582e3fca52f54fec48da473ebfc
Jongheon Park <zxcv4306@gmail.com>
2022-04-12 01:13:39 -0400

    Fixed naming error

--- sources/example/axi_slave_example.sv
+++ sources/example/axi_slave_example.sv
@@ -1,4 +1,5 @@
 // An example AXI slave module
+`timescale 1ns/1ps
 
 module axi_slave_example #(
     parameter ADDR_WIDTH = 0,
@@ -6,7 +7,7 @@ module axi_slave_example #(
 ) (
     input logic clk_i,
     input logic rstn_i,
-    axilITE.Slave axil
+    AXI_LITE axil
 );
     // Internal parameters
     /*
@@ -20,11 +21,11 @@ module axi_slave_example #(
     logic axi_read_ready;
     logic axi_write_ready;
 
-    logic [ADDR_WIDTH-ADDRLSB-1:0] aw_addr,
-                                   ar_addr;
+    logic [ADDR_WIDTH-ADDRLSB-1:0] awaddr,
+                                   araddr;
 
-    assign aw_addr = axil.aw_addr[ADDR_WIDTH-1:ADDRLSB];
-    assign ar_addr = axil.ar_addr[ADDR_WIDTH-1:ADDRLSB];
+    assign awaddr = axil.awaddr[ADDR_WIDTH-1:ADDRLSB];
+    assign araddr = axil.araddr[ADDR_WIDTH-1:ADDRLSB];
 
     // Internal memory -- can be replaced with ram
     logic [DATA_WIDTH-1:0] example_reg0,
@@ -32,20 +33,20 @@ module axi_slave_example #(
                            example_reg2,
                            example_reg3;
 
-    // Default state for ar_ready is high (A3.2.2)
-    //assign axil.ar_ready = 1'b1;
+    // Default state for arready is high (A3.2.2)
+    //assign axil.arready = 1'b1;
 
     // Read
-    always_ff @(posedge clk_i) begin
+    always @(posedge clk_i) begin
         if(!rstn_i) begin
-            axil.r_data <= 0;
-        end else if (!axil.r_valid || axil.r_ready) begin
-            case (ar_addr)
-                2'b00: axil.r_data <= example_reg0;
-                2'b01: axil.r_data <= example_reg1;
-                2'b10: axil.r_data <= example_reg2;
-                2'b11: axil.r_data <= example_reg3;
-                default : axil.r_data <= example_reg0;
+            axil.rdata <= 0;
+        end else if (!axil.rvalid || axil.rready) begin
+            case (araddr)
+                2'b00: axil.rdata <= example_reg0;
+                2'b01: axil.rdata <= example_reg1;
+                2'b10: axil.rdata <= example_reg2;
+                2'b11: axil.rdata <= example_reg3;
+                default : axil.rdata <= example_reg0;
             endcase
         end
     end
@@ -58,30 +59,30 @@ module axi_slave_example #(
             example_reg2 <= 0;
             example_reg3 <= 0;
         end else if (axi_write_ready) begin
-            case (axil.aw_addr)
-                2'b00: example_reg0 <= axil.w_data;
-                2'b01: example_reg1 <= axil.w_data;
-                2'b10: example_reg2 <= axil.w_data;
-                2'b11: example_reg3 <= axil.w_data;
-                default: example_reg0 <= axil.w_data;
+            case (axil.awaddr)
+                2'b00: example_reg0 <= axil.wdata;
+                2'b01: example_reg1 <= axil.wdata;
+                2'b10: example_reg2 <= axil.wdata;
+                2'b11: example_reg3 <= axil.wdata;
+                default: example_reg0 <= axil.wdata;
             endcase
         end
     end
 
     // Read signaling
 
-    assign axi_read_ready = axil.ar_valid && axil.ar_ready;
+    assign axi_read_ready = axil.arvalid && axil.arready;
 
     always_ff @(posedge clk_i) begin
         if (!rstn_i) begin
-            axil.r_valid <= 1'b0; // valid must be cleared following any reset
-        end else if (axil.ar_valid && axil.r_ready)
-            axil.r_valid <= 1'b1;
-        end else if (axil.r_ready)
-            axil.r_valid <= 1'b0;
+            axil.rvalid <= 1'b0; // valid must be cleared following any reset
+        end else if (axil.arvalid && axil.rready)
+            axil.rvalid <= 1'b1;
+        end else if (axil.rready)
+            axil.rvalid <= 1'b0;
     end
 
-    assign axil.ar_ready = !axil.r_valid;
+    assign axil.arready = !axil.rvalid;
 
     // Write signaling
     always_ff @(posedge clk_i) begin
@@ -89,38 +90,38 @@ module axi_slave_example #(
             axi_write_ready <= 1'b0;
         end else begin
             axi_write_ready <= !axi_write_ready 
-                               && (axil.w_valid && axil.aw_valid)
-                               && (axil.b_ready || !axil.b_valid);
+                               && (axil.wvalid && axil.awvalid)
+                               && (axil.bready || !axil.bvalid);
         end
     end
 
-    assign axil.aw_ready = axi_write_ready;
-    assign axil.w_ready = axi_write_ready;
+    assign axil.awready = axi_write_ready;
+    assign axil.wready = axi_write_ready;
 
     // Write response
     always_ff @(posedge clk_i) begin
         if(!rstn_i) begin
-            axil.b_valid <= 1'b0;
+            axil.bvalid <= 1'b0;
         end else if (axi_write_ready) begin
-            axil.b_valid <= 1'b1;
-        end else if (axil.b_ready) begin
-            axil.b_valid <= 1'b0;
+            axil.bvalid <= 1'b1;
+        end else if (axil.bready) begin
+            axil.bvalid <= 1'b0;
         end
     end
 
     // Read response
     always_ff @(posedge clk_i) begin
         if(!rstn_i) begin
-            axil.r_valid <= 1'b0;
+            axil.rvalid <= 1'b0;
         end else if (axi_read_ready) begin
-            axil.r_valid <= 1'b1;
-        end else if (axil.r_ready) begin
-            axil.r_valid <= 1'b0;
+            axil.rvalid <= 1'b1;
+        end else if (axil.rready) begin
+            axil.rvalid <= 1'b0;
         end
     end
 
-    assign axil.b_resp = 2'b00;
-    assign axil.r_resp = 2'b00;
+    assign axil.bresp = 2'b00;
+    assign axil.rresp = 2'b00;
 
     // Strobe
     /*function void funcname();
